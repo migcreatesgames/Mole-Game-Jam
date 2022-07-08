8664c027a6f9d938d9f3838f6444f1180c8aa0ff
Michael Greenwald <michael.greenwald90@gmail.com>
2022-07-08 01:04:20 -0700

    remove commented-out code

--- Project/Mole Game Jam/Assets/Scripts/Worm.cs
+++ Project/Mole Game Jam/Assets/Scripts/Worm.cs
@@ -2,16 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// public class Collectible : MonoBehaviour 
-// {
-//     [SerializeField] protected float _healthPoints = 0;
-//     [SerializeField] protected float _staminaPoints = 0;
-
-//     public float HealthPoints { get => _healthPoints; protected set => _healthPoints = value; }
-//     public float StaminaPoints { get => _staminaPoints; protected set => _staminaPoints = value; }
-
-// }
-
 public class Worm : MonoBehaviour
 {
     

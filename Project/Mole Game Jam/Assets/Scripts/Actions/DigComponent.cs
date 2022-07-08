43462aaf8c8fd2de35df1503c9fc1e6396c042b7
Joshua <gallopingbull89@gmail.com>
2022-07-08 00:38:40 -0700

    refactoring state manager

--- Project/Mole Game Jam/Assets/Scripts/Actions/DigComponent.cs
+++ Project/Mole Game Jam/Assets/Scripts/Actions/DigComponent.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class DigComponent : MonoBehaviour
 {
+    private bool _canDig = false;
+    [SerializeField]
+    private float MAX_digHoldTime;
     [SerializeField]
     private GameObject _holePrefab;
     private DigDetection _detect;
@@ -21,57 +24,44 @@ public class DigComponent : MonoBehaviour
         _detect = GetComponentInChildren<DigDetection>();
     }
 
-    public void Dig(Entity digger)
+    public void Dig(Entity digger, float holdTime)
     {
-        // check if section in front of player can be dug
-        // or far enough from obstables/wall to dig
-        DetectionResults results = _detect.Results;
-        Debug.Log($"results: {_detect.Results}");
-        
-        // init dig action
+        if (_digState == DigStates.digging_complete)
+        {
+            // check if section in front of player can be dug
+            // or far enough from obstables/wall to dig
 
-        SetupDig(results);
-
-        //switch (_digStates)
-        //{
-        //    case DigStates.init_dig:
-        //        break;
-        //    case DigStates.digging:
-        //        break;
-        //    case DigStates.digging_complete:
-        //        
-        //        break;
-        //    default:
-        //        break;
-        //}
+            DetectionResults results = _detect.Results;
+            Debug.Log($"results: {_detect.Results}");
+            // init dig action
+            _canDig = IsValid(results);
+        }
+        if (_canDig)
+        {
+            StateManager(_digState, holdTime);
+        }
     }
 
-    private void SetupDig(DetectionResults results)
+    private void StateManager(DigStates state, float holdTime)
     {
-        switch (results)        
+        switch (state)
         {
-            case DetectionResults.dig_wall:
+            case DigStates.init_dig:
                 // get dig target location
                 // stop player from moving
-                PlayerController.Instance.EnableMovement = true; 
+                //curDigHoldTime += .01f;   
+                GetComponent<Rigidbody>().velocity = Vector3.zero;
+                PlayerController.Instance.EnableMovement = true;
                 // start "setup dig" animation
-
                 // start and increment counter that tracks time that
                 // button is held down
-                break;
-            case DetectionResults.dig_floor:
-                if (_digState == DigComponent.DigStates.digging_complete)
-                {
-                    SpawnDigPrefab();
-                    PlayerController.Instance.EnableMovement = true;
-                    return;
-                }
+             
                 if (_digState != DigStates.digging)
                 {
                     // get dig target location
                     // stop player from moving
                     Transform targetPos = _detect.DigTargetPos;
-                    
+
                     //PlayerController.Instance.GetComponent<Rigidbody>().velocity = Vector3.zero;
                     PlayerController.Instance.EnableMovement = false;
                     //SpawnDigPrefab(targetPos);
@@ -83,35 +73,98 @@ public class DigComponent : MonoBehaviour
                     // start and increment counter that tracks time that
                     // button is held down
                 }
-    
-
-
-                break;  
-            case DetectionResults.not_enough_space:
-                PlayerController.Instance.EnableMovement = true;
-                Debug.Log("warning: not enough space, too close to wall ");
                 break;
-            case DetectionResults.wall:
-
-                PlayerController.Instance.EnableMovement = true;
-                Debug.Log("warning: can't brake through a regular wall");
+            case DigStates.digging:
+                if (holdTime > MAX_digHoldTime)
+                    EnterState(DigStates.digging_complete);
+                
+                if (holdTime < MAX_digHoldTime)
+                {
+                    // continue digging
+                    //if (!anim.isPlaying)
+                    //anim.Play()
+                    Debug.Log("continure digging");
+                    holdTime += .01f;
+                }
+             
                 break;
-            case DetectionResults.hole:
+            case DigStates.digging_complete:
+
+                if (holdTime > MAX_digHoldTime)
+                    SpawnDigPrefab();
+                _canDig = false;
                 PlayerController.Instance.EnableMovement = true;
-                Debug.Log("warning: can't dig next on hole");
+
                 break;
             default:
                 break;
         }
     }
 
-    
+    private void EnterState(DigStates state)
+    {
+        switch (state)
+        {
+            case DigStates.init_dig:
+                break;
+            case DigStates.digging:
+                break;
+            case DigStates.digging_complete:
+                //SpawnDigPrefab();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ExitState(DigStates state)
+    {
+        switch (state)
+        {
+            case DigStates.init_dig:
+                break;
+            case DigStates.digging:
+                break;
+            case DigStates.digging_complete:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private bool IsValid(DetectionResults results)
+    {
+        bool valid = false;
+        switch (results)        
+        {
+            case DetectionResults.dig_wall:
+                valid = true;
+                break;
+            case DetectionResults.dig_floor:
+                valid = true;
+                break;  
+            case DetectionResults.not_enough_space:
+                Debug.Log("warning: not enough space, too close to wall ");
+                break;
+            case DetectionResults.wall:
+                Debug.Log("warning: can't brake through a regular wall");
+                break;
+            case DetectionResults.hole:
+                Debug.Log("warning: can't dig next on hole");
+                break;
+            default:
+                break;
+        }
+
+        return valid;
+    }
+
     public void SpawnDigPrefab()
     {
         var tmp = Instantiate(_holePrefab, _detect._targetPos2) ;
         tmp.transform.parent = null;
         tmp.transform.position = _detect._hitPoint2 -new Vector3(0,.1f,0); 
-        // give random rotation for variation look
+        // give random rotation for visual variation
     }
 }
 
--- Project/Mole Game Jam/Assets/Scripts/PlayerController.cs
+++ Project/Mole Game Jam/Assets/Scripts/PlayerController.cs
@@ -13,7 +13,6 @@ public class PlayerController : Entity
     private InputHandler _inputHandler;
     private MovementComponent _movementComponent;
     private DigComponent _digComponent;
-
     private CarryComponent _carryComponent;
 
     public static PlayerController Instance { get => _instance; set => _instance = value; }
@@ -46,38 +45,58 @@ public class PlayerController : Entity
             }
             if (Input.GetButton("Dig"))
             {
-        
                 Debug.Log("holding Dig Button");
                 Debug.Log($"dig state: {_digComponent.DigState}");
                 Debug.Log($"curDigHoldTime : {curDigHoldTime}");
-                
-                if (curDigHoldTime > MAXDigHoldTime && _digComponent.DigState == DigComponent.DigStates.digging)
+                _digComponent.Dig(this, curDigHoldTime);
+
+                // stop digging
+                if (curDigHoldTime > MAXDigHoldTime)
                 {
-                    // stop digging
-                    curDigHoldTime = 0f;
-                    _digComponent.DigState = DigComponent.DigStates.digging_complete;
-                    _digComponent.Dig(this);
+
                 }
-                if (curDigHoldTime == 0 && _digComponent.DigState == DigComponent.DigStates.digging_complete)
+                // start digging
+                if (curDigHoldTime == 0)
                 {
-                    // start digging
-                    curDigHoldTime += .01f;
-                    GetComponent<Rigidbody>().velocity = Vector3.zero;
-                    _digComponent.DigState = DigComponent.DigStates.init_dig;
-                    _digComponent.Dig(this);
+
                 }
-                if (curDigHoldTime < MAXDigHoldTime && _digComponent.DigState == DigComponent.DigStates.digging)
+                // continue digging
+                if (curDigHoldTime < MAXDigHoldTime)
                 {
-                    // continue digging
-                    Debug.Log("continure digging");
-                    curDigHoldTime += .01f;
-                    _digComponent.Dig(this);
-                }     
+
+                }
+                //if (curDigHoldTime > MAXDigHoldTime && 
+                //    _digComponent.DigState == DigComponent.DigStates.digging)
+                //{
+                //    // stop digging
+                //    curDigHoldTime = 0f;
+                //    _digComponent.DigState = DigComponent.DigStates.digging_complete;
+                //    _digComponent.Dig(this, curDigHoldTime);
+                //}
+                //if (curDigHoldTime == 0 && 
+                //    _digComponent.DigState == DigComponent.DigStates.digging_complete)
+                //{
+                //    // start digging
+                //    curDigHoldTime += .01f;
+                //    GetComponent<Rigidbody>().velocity = Vector3.zero;
+                //    _digComponent.DigState = DigComponent.DigStates.init_dig;
+                //    _digComponent.Dig(this, curDigHoldTime);
+                //}
+                //if (curDigHoldTime < MAXDigHoldTime && 
+                //    _digComponent.DigState == DigComponent.DigStates.digging)
+                //{
+                //    // continue digging
+                //    Debug.Log("continure digging");
+                //    curDigHoldTime += .01f;
+                //    _digComponent.Dig(this, curDigHoldTime);
+                //}     
             }
         }
 
-        if (Input.GetButtonUp("Dig"))
+        if (Input.GetButtonUp("Dig") && 
+            _digComponent.DigState == DigComponent.DigStates.digging)
         {
+            // stop digging
             curDigHoldTime = 0f;
             _digComponent.DigState = DigComponent.DigStates.digging_complete;
             PlayerController.Instance.EnableMovement = true;
@@ -89,7 +108,5 @@ public class PlayerController : Entity
         Speed = _carryComponent.RunSpeedCarryingWorms;
         _movementComponent.Move(_xInput, _yInput, Speed);
     }
-
-    
 }
 

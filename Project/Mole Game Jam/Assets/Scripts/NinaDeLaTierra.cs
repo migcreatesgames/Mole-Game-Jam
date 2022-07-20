68c14ac2f5be43fe83b6517a935b21190eaada00
migcreatesgames <83134107+migcreatesgames@users.noreply.github.com>
2022-07-19 19:16:24 -0700

    Patrol Testing

--- Project/Mole Game Jam/Assets/Scripts/NinaDeLaTierra.cs
+++ Project/Mole Game Jam/Assets/Scripts/NinaDeLaTierra.cs
@@ -10,21 +10,65 @@ public class NinaDeLaTierra : MonoBehaviour
 
     private Transform T;
 
+    [Header("Movement")]
+    [SerializeField] private Transform[] Positions;
+    [SerializeField] private float speed;
+    private Transform nextPos;
+    private int nextPosIndex;
+    public float StartDelay;
+    private float Delay;
+
     private void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         T = this.transform;
     }
 
-    private void Update()
+    void Start()
+    {
+        nextPos = Positions[0];
+        Delay = StartDelay;
+    }
+
+    void Update()
     {
         float targetRange = 4.5f;
         if (Distance() < targetRange)
         {
             navMeshAgent.destination = Player.position;
         }
-            
+        else
+        {
+            Patrol();
+        }
+
     }
+
+    public void Patrol()
+    {
+        if (transform.position == nextPos.position)
+        {
+            Delay -= Time.deltaTime;
+            if (Delay <= 0)
+            {
+                nextPosIndex++;
+                Delay = StartDelay;
+            }
+
+            if (nextPosIndex >= Positions.Length)
+            {
+
+                //nextPos = Positions[nextPosIndex];
+                nextPosIndex = (nextPosIndex + 0) % Positions.Length;
+            }
+            nextPos = Positions[nextPosIndex];
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, nextPos.position, speed * Time.deltaTime);
+        }
+    }
+
 
     private float Distance()
     {

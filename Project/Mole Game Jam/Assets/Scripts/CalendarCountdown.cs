192ffa67d95d1e6bc1edd7785d62da6c5c14586a
Joshua <gallopingbull89@gmail.com>
2022-07-20 22:11:05 -0700

    more calander update

--- Project/Mole Game Jam/Assets/Scripts/CalendarCountdown.cs
+++ Project/Mole Game Jam/Assets/Scripts/CalendarCountdown.cs
@@ -3,7 +3,7 @@ using TMPro;
 
 public class CalendarCountdown : MonoBehaviour
 {
-    public float MaxTime = 500f; // let's keep this around 5 mins max
+    public float MaxTime = 100000000f; // let's keep this around 5 mins max
     private float currentTime = 0;
 
     private bool _enableFade = false;
@@ -25,19 +25,29 @@ public class CalendarCountdown : MonoBehaviour
     {
         if (currentTime < MaxTime)
         {
-            currentTime--;
+            Debug.Log($"current time: {currentTime}");
+            currentTime += .15f;
             ProgressTime(currentTime);
         }
 
+        Debug.Log($"fade: {_enableFade}");
         if (_enableFade)
         {
-
+            if (textTarget.alpha != 0)
+            {
+                textTarget.alpha -= .01f;
+            }
+            else
+            {
+                _enableFade = false;
+            }
         }
     }
 
     private void ProgressTime(float time) 
     {
-        switch (time)
+        int tmpTime = Mathf.RoundToInt(time);
+        switch (tmpTime)
         {
             case 0:
                 // game start
@@ -45,13 +55,14 @@ public class CalendarCountdown : MonoBehaviour
                 if (season != Seasons.spring)
                 {
                     season = Seasons.spring;
+                    _enableFade = true;
                     FadeOutText(season);
                 }
                    
 
                 break;
 
-            case 30:
+            case 100:
                 // is summer
                 if (season != Seasons.summer)
                 {
@@ -60,7 +71,7 @@ public class CalendarCountdown : MonoBehaviour
                 }
                 break;
             
-            case 60:
+            case 300:
                 // is fall
                 if (season != Seasons.fall)
                 {
@@ -68,7 +79,7 @@ public class CalendarCountdown : MonoBehaviour
                     FadeOutText(season);
                 }
                 break;
-            case 90:
+            case 600:
                 // is Winter
                 if (season != Seasons.fall)
                 {
@@ -87,15 +98,19 @@ public class CalendarCountdown : MonoBehaviour
         {
             case Seasons.winter:
                 textTarget = SeasonNames[0];
+                textTarget.alpha = 100;
                 break;
             case Seasons.spring:
                 textTarget = SeasonNames[1];
+                textTarget.alpha = 100;
                 break;
             case Seasons.fall:
                 textTarget = SeasonNames[2];
+                textTarget.alpha = 100;
                 break;
             case Seasons.summer:
                 textTarget = SeasonNames[3];
+                textTarget.alpha = 100;
                 break;
             default:
                 break;

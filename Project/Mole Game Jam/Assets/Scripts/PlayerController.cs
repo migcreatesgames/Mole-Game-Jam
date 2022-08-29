7a42caaf7609d1c6c3654388a98f5b6c94f1af66
Joshua <gallopingbull89@gmail.com>
2022-08-28 17:13:19 -0700

    art changes

--- Project/Mole Game Jam/Assets/Scripts/PlayerController.cs
+++ Project/Mole Game Jam/Assets/Scripts/PlayerController.cs
@@ -311,7 +311,6 @@ public class PlayerController : Entity
                     GetComponent<DustTrail>().EnableDustTrails = false;
                 if (_encumbered)
                     _animator.SetBool("Encumbered", true);
-                
                 break;
 
             default:

f0637e1c1c41b659c5a055fa14f00d0c8030dde3
Joshua <gallopingbull89@gmail.com>
2022-09-07 21:19:11 -0700

    adding swapping algorithm to procedural generation editor tool

--- Project/Mole Game Jam/Assets/Editor/ProcGenEditorTool.cs
+++ Project/Mole Game Jam/Assets/Editor/ProcGenEditorTool.cs
@@ -1,29 +1,55 @@
 using UnityEditor;
 using UnityEngine;
+using System;
 
+[Serializable]
 public class ProcGenEditorTool : EditorWindow
 {
-    static EditorWindow _window;
-    private GameObject[] _blocks; 
+    private static EditorWindow _window;
+
+    private static string[] _assetSearchFolders;
+    private static int[] _possibleValues = new int[] {0, 1, 0, 2, 0, 3, 0, 4, 0, 5};
+    private static GameObject[] _blocks;
+    private static ProceduralGeneration _blockData;
     
-    [MenuItem("Tools/Swap Block", false, -1)]
+    [MenuItem("Tools/Swap Block", false, -1)]  
     private static void Init()
     {
         _window = GetWindow(typeof(ProcGenEditorTool));
         _window.Show();
+        
+        _assetSearchFolders = new string[1];
+        _assetSearchFolders[0] = "Assets/Scripts/ScriptableObjects";
+
+        string[] SO_GUIDs = AssetDatabase.FindAssets("t:ProceduralGeneration", _assetSearchFolders);
+        var SOpath = AssetDatabase.GUIDToAssetPath(SO_GUIDs[0]);
+        Debug.Log($"path: {SOpath}");
+
+        _blockData = (ProceduralGeneration)AssetDatabase.LoadAssetAtPath(SOpath, typeof(ProceduralGeneration));
+        _blocks = _blockData.Blocks;
+        
+        for (int i = 0; i < _blocks.Length; i++)
+            Debug.Log($"_blocks[{i}] = {_blocks[i].name}");
     }
 
     public static void SwapBlocks()
     {
         GameObject[] blocks = GameObject.FindGameObjectsWithTag("Wall");
         for (int i = 0; i < blocks.Length; i++)
+        {
             blocks[i] = GenerateNewBlock(blocks[i].transform);
+
+        }
     }
 
     private static GameObject GenerateNewBlock(Transform target)
     {
         // get new block
-        target.gameObject.GetComponent<MeshRenderer>();
+        int index = UnityEngine.Random.Range(0, _possibleValues.Length); 
+        GameObject tmpBlock = Instantiate(_blocks[_possibleValues[index]], target);
+        tmpBlock.transform.position = Vector3.zero;
+
+        //target.gameObject.GetComponentInChildren<MeshRenderer>();
         // destroy old block mesh
 
 
@@ -43,5 +69,4 @@ public class ProcGenEditorTool : EditorWindow
         GUILayout.Button("Randomize Bricks");
         //EditorGUILayout.TextArea("value", );
     }
-
 }
--- Project/Mole Game Jam/Assets/Scripts/ProceduralGeneration.cs
+++ Project/Mole Game Jam/Assets/Scripts/ProceduralGeneration.cs
@@ -1,14 +1,9 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "BlockProcGenData", menuName = "ScriptableObjects/BlockProcGenDatas")]
+[CreateAssetMenu(fileName = "BlockProcGenData", 
+    menuName = "ScriptableObjects/BlockProcGenDatas")]
 public class ProceduralGeneration : ScriptableObject
 {
     public GameObject[] Blocks;
-    public GameObject[] GetBlock()
-    {
-        // var randBlock = GetRandomint;
-
-
-        return null;
-    }
+ 
 }

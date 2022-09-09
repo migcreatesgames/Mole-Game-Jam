e04822ee8e0ffcd7ae1157f3772384c3c0f36f8e
Joshua <gallopingbull89@gmail.com>
2022-09-08 21:11:08 -0700

    finished block generator tool

--- Project/Mole Game Jam/Assets/Editor/ProcGenEditorTool.cs
+++ Project/Mole Game Jam/Assets/Editor/ProcGenEditorTool.cs
@@ -5,67 +5,88 @@ using System;
 [Serializable]
 public class ProcGenEditorTool : EditorWindow
 {
+
     private static EditorWindow _window;
 
-    private static string[] _assetSearchFolders;
     private static int[] _possibleValues = new int[] {0, 1, 0, 2, 0, 3, 0, 4, 0, 5};
     private static GameObject[] _blocks;
     private static ProceduralGeneration _blockData;
-    
-    [MenuItem("Tools/Swap Block", false, -1)]  
-    private static void Init()
+    /// <summary>
+    /// 
+    /// </summary>
+    public void OnGUI()
     {
-        _window = GetWindow(typeof(ProcGenEditorTool));
-        _window.Show();
-        
-        _assetSearchFolders = new string[1];
-        _assetSearchFolders[0] = "Assets/Scripts/ScriptableObjects";
-
-        string[] SO_GUIDs = AssetDatabase.FindAssets("t:ProceduralGeneration", _assetSearchFolders);
-        var SOpath = AssetDatabase.GUIDToAssetPath(SO_GUIDs[0]);
-        Debug.Log($"path: {SOpath}");
-
-        _blockData = (ProceduralGeneration)AssetDatabase.LoadAssetAtPath(SOpath, typeof(ProceduralGeneration));
-        _blocks = _blockData.Blocks;
-        
-        for (int i = 0; i < _blocks.Length; i++)
-            Debug.Log($"_blocks[{i}] = {_blocks[i].name}");
-
+        EditorGUILayout.LabelField("Level", "hello");
+        if (GUILayout.Button("Randomize Bricks"))
+            SwapBlocks();
+        if (GUILayout.Button("Delete All Bricks"))
+            DeleteAllBlocks();
     }
 
+    [MenuItem("Tools/Generate Random Blocks", false, -1)]  
+    private static void Init()
+    {
+        GetWindow(typeof(ProcGenEditorTool));
+    }
+
+    // This method will be called on load or recompile
+    [InitializeOnLoadMethod]
+    private static void OnLoad()
+    {
+        // if no data exists yet create and reference a new instance
+        if (!_blockData)
+        {
+            var SOpath = GetPathToBlocksScriptableObject();
+
+            _blockData = (ProceduralGeneration)AssetDatabase.LoadAssetAtPath(SOpath, typeof(ProceduralGeneration));
+            _blocks = _blockData.Blocks;
+
+            Init();
+        }
+    }
     public static void SwapBlocks()
     {
         GameObject[] blocks = GameObject.FindGameObjectsWithTag("Wall");
-        for (int i = 0; i < blocks.Length; i++)
-        {
-            GenerateNewBlock(blocks[i].transform);
-        }
+
+        // only swap blocks if first wall block has no child objects
+        if (blocks[0].transform.childCount == 0)
+            for (int i = 0; i < blocks.Length; i++)
+                GenerateNewBlock(blocks[i].transform);
     }
 
     private static void GenerateNewBlock(Transform target)
     {
-        // get new block
         int index = UnityEngine.Random.Range(0, _possibleValues.Length);
-        DestroyImmediate(target.Find("standardBlock01"));
+
         GameObject tmpBlock = Instantiate(_blocks[_possibleValues[index]], target);
-        tmpBlock.transform.position = Vector3.zero;
 
-        //target.gameObject.GetComponentInChildren<MeshRenderer>();
-        // destroy old block mesh
-
+        tmpBlock.transform.localPosition = Vector3.zero;
+        tmpBlock.transform.localRotation = GenerateNewRotation();
     }
 
-    private Vector2 GenerateNewRotation()
+    private static void DeleteAllBlocks()
     {
-        return new Vector2(0, 0);
+        GameObject[] blocksMeshes = GameObject.FindGameObjectsWithTag("BlockMesh");
+
+        if (blocksMeshes.Length > 0)
+            for (int i = 0; i < blocksMeshes.Length; i++)
+                DestroyImmediate(blocksMeshes[i]);
     }
 
-
-    public void OnGUI()
+    private static Quaternion GenerateNewRotation()
     {
-        //myTarget.experience = EditorGUILayout.IntField("Experience", myTarget.experience);
-        EditorGUILayout.LabelField("Level","hello");
-        GUILayout.Button("Randomize Bricks");
-        //EditorGUILayout.TextArea("value", );
+        float x = UnityEngine.Random.Range(0, 4) * 90;
+        float y = UnityEngine.Random.Range(0, 4) * 90;
+        float z = UnityEngine.Random.Range(0, 4) * 90;
+        return Quaternion.Euler(x, y, z);
+    }
+
+    private static string GetPathToBlocksScriptableObject()
+    {
+        string[] _assetSearchFolders = new string[1];
+        _assetSearchFolders[0] = "Assets/Scripts/ScriptableObjects";
+
+        string[] SO_GUIDs = AssetDatabase.FindAssets("t:ProceduralGeneration", _assetSearchFolders);
+        return AssetDatabase.GUIDToAssetPath(SO_GUIDs[0]);
     }
 }
